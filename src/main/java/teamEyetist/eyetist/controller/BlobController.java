38105ce35516604b591539662a90beb5841f0626
조준희 <iopp3423@gmail.com>
2022-12-31 23:47:26 +0900

    [UPDATE] 컨테이너 생성 후 blob에 이미지 저장하는 코드로 업데이트:

--- src/main/java/teamEyetist/eyetist/controller/BlobController.java
+++ src/main/java/teamEyetist/eyetist/controller/BlobController.java
@@ -1,8 +1,8 @@
 package teamEyetist.eyetist.controller;
 
-import com.azure.storage.blob.BlobClient;
-import com.azure.storage.blob.BlobContainerClient;
-import com.azure.storage.blob.BlobContainerClientBuilder;
+import com.azure.identity.DefaultAzureCredential;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.storage.blob.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.WritableResource;
@@ -20,7 +20,7 @@ import java.util.Date;
 @RestController
 @RequestMapping("blob")
 public class BlobController{
-    @Value("https://eyetiststorage.blob.core.windows.net/images/hi")
+    @Value("https://eyetiststorage.blob.core.windows.net/test/testtest.png")
     private Resource resource;
 
     @GetMapping("/readBlobFile")
@@ -38,18 +38,33 @@ public class BlobController{
     }
     @PostMapping("/test")
     public String writeTest(@RequestPart(value = "file") MultipartFile file) throws IOException{
-        // Create a unique name for the container
-        String containerName = "quickstartblobs" + java.util.UUID.randomUUID();
+
+        //Azure에 로그인할 디폴트 크레덴셜
+        DefaultAzureCredential defaultCredential = new DefaultAzureCredentialBuilder().build();
+
+        //Azure에 로그인
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+                .endpoint("https://eyetiststorage.blob.core.windows.net/")
+                .credential(defaultCredential)
+                .buildClient();
+
+        // 컨테이너 생성
+        String name = "test";
+        // 컨테이너 생성
+        blobServiceClient.createBlobContainer(name);
 
         BlobContainerClient container = new BlobContainerClientBuilder()
                 .connectionString("DefaultEndpointsProtocol=https;AccountName=eyetiststorage;AccountKey=SkhZFLZGXwefVTMjkwqAyQgdNZpz3eA1nCCHfshtn4/xdAYwQbRNKFVRlMxINVWdtWKOzYrD6PJH+AStbSGvYQ==;EndpointSuffix=core.windows.net")
-                .containerName("images")
+                .containerName(name)
                 .buildClient();
 
-        BlobClient blobClient = container.getBlobClient("test");
+        // 파일 객체의 파일을 Blob 컨테이너에 할당
+        BlobClient blobClient = container.getBlobClient("testtest.png");
+
+        // Upload the blob
         blobClient.upload(file.getInputStream(), file.getSize(), true);
 
         System.out.print("파일 업로드 성공");
-        return containerName;
+        return name;
     }
 }
