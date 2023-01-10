3213525bde452a9fcb4e1406a4ee87e4050b2e32
조준희 <iopp3423@gmail.com>
2023-01-10 20:05:07 +0900

    [ADD] 플러그인 추가

--- src/main/java/teamEyetist/eyetist/EyetistApplication.java
+++ src/main/java/teamEyetist/eyetist/EyetistApplication.java
@@ -3,12 +3,16 @@ package teamEyetist.eyetist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EyetistApplication {
+public class EyetistApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EyetistApplication.class, args);
 	}
-
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EyetistApplication.class);
+	}
 }
