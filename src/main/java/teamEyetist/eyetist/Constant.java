4c857a1e8709b5f797a29f0c72f9f9fd14a85957
jun <48996701+iopp3423@users.noreply.github.com>
2023-01-25 16:27:07 +0900

    Update Constant.java

--- src/main/java/teamEyetist/eyetist/Constant.java
+++ src/main/java/teamEyetist/eyetist/Constant.java
@@ -1,5 +1,5 @@
 package teamEyetist.eyetist;
 
 public class Constant {
-    public static final String CONNECT_STRING = "DefaultEndpointsProtocol=https;AccountName=eyetiststorage;AccountKey=GS/r8XlAh96ulBFtlicC+FYL+IQXOKksbLcvphHfN2ti1pVZn/4tJCpRoAGDWWXD+9/WwOVaIc9Z+AStQiCrZw==;EndpointSuffix=core.windows.net";
+    public static final String CONNECT_STRING = "AZURE_CONNECT_STRING";
 }
